90ac81316997ca7aac28d7982efb74adf93a15bf
ivankana <ivankana@hotmail.com>
2015-04-20 22:53:04 +0200

    Fekk til colorPicker for rammen, Woohoo!!

--- src/Border.java
+++ src/Border.java
@@ -1,5 +1,5 @@
 import processing.core.PApplet;
-import javax.swing.*;
+
 
 /**
  * Created by Ivan on 20.04.2015.
@@ -7,9 +7,11 @@ import javax.swing.*;
 public class Border {
     PApplet p;
     Application ap;
-    JFrame frame = new JFrame("Input");
+    ColorPicker colorPicker = new ColorPicker();
+
     int state = 0;
     int stokeSize = 30;
+
 
     Border(PApplet input, Application a){
         p = input;
@@ -20,51 +22,15 @@ public class Border {
     public void drawBorder(){
         if (state == 0) {
 
-            // prompt the user to enter their name
-            String[] color = {"Hvit", "Svart", "Rød", "Blå", "Grøn", "Lilla", "Oransj", "Gul"};
+            colorPicker.color();
 
 
-            String pickedColor = (String) JOptionPane.showInputDialog(frame,
-                    "What color border do you want?",
-                    "Color Picker",
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    color,
-                    color[0]);
+                p.strokeWeight(stokeSize);
+                p.stroke(colorPicker.getR(), colorPicker.getG(), colorPicker.getB());
 
-
-            // favoritePizza will be null if the user clicks Cancel
-            System.out.printf("Picked color %s.\n", pickedColor);
-
-            p.strokeWeight(stokeSize);
-            if (pickedColor == "Hvit") {
-                p.stroke(255);
-            } else if (pickedColor == "Svart") {
-                p.stroke(0);
-            } else if (pickedColor == "Rød") {
-                p.stroke(255, 0, 0);
-            } else if (pickedColor == "Blå") {
-                p.stroke(0, 0, 255);
-            } else if (pickedColor == "Grøn") {
-                p.stroke(0, 255, 0);
-            } else if (pickedColor == "Lilla") {
-                p.stroke(161, 0, 236);
-            } else if (pickedColor == "Oransj") {
-                p.stroke(236, 128, 0);
-            } else if (pickedColor == "Gul") {
-                p.stroke(255, 255, 0);
-            }
-
-
-            //System.exit(0);
-
-
-
-
-            p.noFill();
-            //rectMode(CENTER);
-            p.rect(13, 10, ap.panel.getWidth() - stokeSize, ap.panel.getHeight() - stokeSize);
-
+                p.noFill();
+                //rectMode(CENTER);
+                p.rect(13, 10, ap.panel.getWidth() - stokeSize, ap.panel.getHeight() - stokeSize);
 
         }
         state = 1;
