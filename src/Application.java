a685bb10d0a116e27ec432cf0772ff4213a793f3
Urdar <jens.vingen@gmail.com>
2015-04-22 14:51:59 +0200

    Fjernet overflødig kode + javadoc kommentering

--- src/Application.java
+++ src/Application.java
@@ -3,177 +3,183 @@ import java.util.HashMap;
 import javax.swing.*;
 import javax.swing.BorderFactory;
 
-
 /**
- * A simple demo application launching a Processing Applet
- * <p/>
- * Demonstrates the combination of JFrame, JButton, JFileChooser
- * and PApplet.
- *
- * @author georg munkel
+ * Main class for ArtGen. Initiate to create objects and run GenArt
+ * @author  Gruppe 6
+ * @version 1.0, April 2015
  */
 
 public class Application {
-    private int strokeSize = 1;
+
+    //private static final JCheckBox circular = new JCheckBox("Sirkulær");
     private static final JCheckBox randomclr = new JCheckBox("Tilfeldig farge");
+
+    //private static final JCheckBox linear = new JCheckBox("Lineær");
     private static final JCheckBox border = new JCheckBox("Ramme");
+
+    // Width for right panel, menupanel
     private static final int menuWidth = 200; // Husk å endre i MyApplet hvis du endrer her
+
+    // panel holds Processing applet, buttonPanel holds menu items
     public static JPanel panel, buttonPanel;
 
+    // For availability, declare all buttons
     private static JButton vectorButton, clearButton, randomLinesButton, pulseButton, crossDotsButton,
-            starButton, heartButton, squarezButton,  buttonCreate, varBubblesButton, trianglezButton,
-            Randomize,strokeNColourButton, filterButton, saveButton, closeButton, signatureButton, printButton;
+            starButton, heartButton, squarezButton, buttonCreate, varBubblesButton, trianglezButton,
+            Randomize, strokeNColourButton, filterButton, saveButton, closeButton, signatureButton;
 
+    // Hashmap to support "Tormod"
     public static HashMap<String, JButton> buttonMap = new HashMap();
 
-    public Application(){}
+    public Application() {
+    }
 
     public static void main(String[] args) {
-        //create a frame for the application
+        // Create a JFrame for the application
         final JFrame frame = new JFrame("Fantastic Art Generator");
         frame.setExtendedState(Frame.MAXIMIZED_BOTH);
         frame.setBackground(Color.white);
         frame.setLayout(null);
 
+        // For "true fullscreen". Can be set to false
+        frame.setUndecorated(true);
 
-
-
-        frame.setUndecorated(true); // Aktiver for å fjerne tittel etc, "skikkelig" fullskjerm
-//make sure to shut down the application, when the frame is closed
+        // To ensure proper shutdown upon exit
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-//create a panel for the applet and the button panel
+        // Create a JPanel for the Processing applet, drawing area
         panel = new JPanel();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int panelXChange = screenSize.width - menuWidth;
-        panel.setBounds(0,0,panelXChange,screenSize.height);
-
+        panel.setBounds(0, 0, panelXChange, screenSize.height);
         panel.setBackground(Color.white);
 
-
-//create a panel for the buttons
+        // Create a JPanel for menu items
         buttonPanel = new JPanel();
         buttonPanel.setBackground(Color.white);
-
-        buttonPanel.setBounds(panelXChange,0,menuWidth,screenSize.height);
-        System.out.println(screenSize.width);
-        System.out.println(screenSize.height);
-        System.out.println(panelXChange);
-
-// svart border til venstre for knapper
+        buttonPanel.setBounds(panelXChange, 0, menuWidth, screenSize.height);
         buttonPanel.setBorder(BorderFactory.createMatteBorder(0, 1, 0, 0, Color.LIGHT_GRAY));
 
 
-//create an instance of your processing applet
+        // Create an instance of Processing applet
         final MyApplet applet = new MyApplet();
 
-//start the applet
+        // Start the applet
         applet.init();
 
-//Buttons
-//create a button labled "create new ball"
-
+        // Create buttons
+        // Ball grid
         ImageIcon imageForbuttonCreate = new ImageIcon("images/ballgrid.png");
         buttonCreate = new JButton("", imageForbuttonCreate);
         buttonCreate.setBackground(Color.white);
         buttonCreate.setPreferredSize(new Dimension(90, 90));
 
-
+        // Round vector
         ImageIcon imageForvectorButton = new ImageIcon("images/roundvector.png");
         vectorButton = new JButton("", imageForvectorButton);
         vectorButton.setBackground(Color.white);
         vectorButton.setPreferredSize(new Dimension(90, 90));
 
-
+        // Reset drawing area
         clearButton = new JButton("Reset");
+
+        // Randomized, automatic drawing, "Tormod"
         Randomize = new JButton("Randomisert");
         Randomize.setBackground(Color.GREEN);
         Randomize.setForeground(Color.WHITE);
         Randomize.setBorderPainted(true);
-        //Randomize.setBorder(null);
+
+        // Save
         saveButton = new JButton("Lagre");
+
+        // Close program
         closeButton = new JButton("Lukk Programmet");
+
+        // Filters
         filterButton = new JButton("Filter");
 
+        // Random lines
         ImageIcon imageForrandomLinesButton = new ImageIcon("images/rndlines.png");
         randomLinesButton = new JButton("", imageForrandomLinesButton);
         randomLinesButton.setBackground(Color.white);
         randomLinesButton.setPreferredSize(new Dimension(90, 90));
 
+        // Bubbles
         ImageIcon imageForvarBubblesButton = new ImageIcon("images/varbubbles.png");
-        varBubblesButton = new JButton("",imageForvarBubblesButton);
+        varBubblesButton = new JButton("", imageForvarBubblesButton);
         varBubblesButton.setBackground(Color.white);
         varBubblesButton.setPreferredSize(new Dimension(90, 90));
 
-
+        // Pulse
         ImageIcon imageForpulseButton = new ImageIcon("images/pulse.png");
-        pulseButton = new JButton("",imageForpulseButton);
+        pulseButton = new JButton("", imageForpulseButton);
         pulseButton.setBackground(Color.white);
         pulseButton.setPreferredSize(new Dimension(90, 90));
 
+        // CrossDots
         ImageIcon imageForcrossDotsButton = new ImageIcon("images/crossdots.png");
         crossDotsButton = new JButton("", imageForcrossDotsButton);
         crossDotsButton.setBackground(Color.white);
         crossDotsButton.setPreferredSize(new Dimension(90, 90));
 
-        ImageIcon imageForPrintButton = new ImageIcon("images/printer1.png");
-        printButton = new JButton("", imageForPrintButton);
-        printButton.setBackground(Color.white);
-        printButton.setPreferredSize(new Dimension(90, 90));
-
-        ImageIcon imageForStarButton = new ImageIcon("images/star.png");
+        // Starbutton
+        ImageIcon imageForStarButton = new ImageIcon("images/printer1.png");
         starButton = new JButton("", imageForStarButton);
         starButton.setBackground(Color.white);
         starButton.setPreferredSize(new Dimension(90, 90));
 
+        // Mirror
+        // TODO refactor heartButton (expired) to mirrorButton
         ImageIcon imageForHeartButton = new ImageIcon("images/mirrorvector.png");
         heartButton = new JButton("", imageForHeartButton);
         heartButton.setBackground(Color.white);
         heartButton.setPreferredSize(new Dimension(90, 90));
 
-        ImageIcon imageForsquarezButton = new ImageIcon("images/sq.png");
+        // Squarez
+        ImageIcon imageForsquarezButton = new ImageIcon("images/squarez.png");
         squarezButton = new JButton("", imageForsquarezButton);
         squarezButton.setBackground(Color.white);
         squarezButton.setPreferredSize(new Dimension(90, 90));
 
+        // Triangles
         ImageIcon imageFortrianglezButton = new ImageIcon("images/trianglez.png");
         trianglezButton = new JButton("", imageFortrianglezButton);
         trianglezButton.setBackground(Color.white);
         trianglezButton.setPreferredSize(new Dimension(90, 90));
 
-        ImageIcon imageForStrokeNColourButton = new ImageIcon("images/cp.png");
+        // Color chooser
+        ImageIcon imageForStrokeNColourButton = new ImageIcon("images/strokencolour80x80.png");
         strokeNColourButton = new JButton("", imageForStrokeNColourButton);
         strokeNColourButton.setBackground(Color.white);
         strokeNColourButton.setPreferredSize(new Dimension(90, 90));
 
+        // Signature
         ImageIcon imageForSignatureButton = new ImageIcon("images/signature.png");
         signatureButton = new JButton("", imageForSignatureButton);
         signatureButton.setBackground(Color.white);
         signatureButton.setPreferredSize(new Dimension(90, 90));
 
-//assing a tooltip
-        buttonCreate.setToolTipText("Lager baller som går i forskjellige retninger.");
-        vectorButton.setToolTipText("Lager en mengde vektorer som beveger seg i sirkel.");
-        clearButton.setToolTipText("Fjerner alt på lerretet.");
-        filterButton.setToolTipText("Tilfør et filter.");
-        randomLinesButton.setToolTipText("Tegner linjer i forskjellig størrelse.");
-        varBubblesButton.setToolTipText("Tegner bobler som varierer etter musepeker-hastigheten.");
-        pulseButton.setToolTipText("Tegner sirkler som pulserer i forskjellige størrelse.");
-        crossDotsButton.setToolTipText("Tegner 'dotter' som går i kryss formasjon.");
-        saveButton.setToolTipText("Lagrer bildet ditt på skrivebordet.");
-        closeButton.setToolTipText("Lukk programmet.");
-        printButton.setToolTipText("Printer ut bildet.");
-        starButton.setToolTipText("Tegner stjerner.");
-        heartButton.setToolTipText("Tegner hjerter.");
-        squarezButton.setToolTipText("Tegner firkanter i forskjellig størrelse.");
-        trianglezButton.setToolTipText("Tegner trekanter i forskjellig vinkel og størrelse.");
-        strokeNColourButton.setToolTipText("Velger farge på penslene dine.");
-        signatureButton.setToolTipText("Legg igjen din signatur på bildet.");
-        // Adding button graphics
+
+        // Assing tooltip to buttons
+        buttonCreate.setToolTipText("creates a new ball ");
+        vectorButton.setToolTipText("creates a new vector");
+        clearButton.setToolTipText("clears the screen");
+        filterButton.setToolTipText("add a filter");
+        randomLinesButton.setToolTipText("creates random lines");
+        varBubblesButton.setToolTipText("Draws random size bubbles depending on mouse speed");
+        pulseButton.setToolTipText("Pulsing");
+        crossDotsButton.setToolTipText("Draws dots in cross formation");
+        saveButton.setToolTipText("Draws dots in cross formation");
+        closeButton.setToolTipText("Lukk programmet");
+        starButton.setToolTipText("Print out your art");
+        heartButton.setToolTipText("Draw mirror vectors");
+        squarezButton.setToolTipText("Draws squares as you drag your mouse");
+        trianglezButton.setToolTipText("Draws triangles as you drag your mouse");
+        strokeNColourButton.setToolTipText("Choose stroke size and colours");
+        signatureButton.setToolTipText("Add your signature");
 
 
-//give a name for the command
-//if this is not assigned the actionCommand equals the button label
+        // Set names for buttons in order to initiate ActionCommand
         buttonCreate.setActionCommand("create ball");
         vectorButton.setActionCommand("create vector");
         clearButton.setActionCommand("clear");
@@ -191,9 +197,8 @@ public class Application {
         Randomize.setActionCommand("randomize");
         strokeNColourButton.setActionCommand("strokencolour");
         signatureButton.setActionCommand("signature");
-        printButton.setActionCommand("printing");
 
-        //Add buttons to map. Name of button is key, while actual button is the value.
+        // Add buttons to map. Name of button is key, while actual button is the value.
         buttonMap.put("buttonCreate", buttonCreate);
         buttonMap.put("vectorButton", vectorButton);
         buttonMap.put("randomLinesButton", randomLinesButton);
@@ -211,11 +216,9 @@ public class Application {
         buttonMap.put("clearButton", clearButton);
         buttonMap.put("filterButton", filterButton);
         buttonMap.put("signatureButton", signatureButton);
-        buttonMap.put("printButton", printButton);
-
 
         //Iterate through entrySet, and set class Applet as action listener for every button.
-        for(Object button : buttonMap.keySet()){
+        for (Object button : buttonMap.keySet()) {
             String nameOfButton = button.toString();
             JButton t = buttonMap.get(nameOfButton);
             t.addActionListener(applet);
@@ -223,10 +226,11 @@ public class Application {
 
         border.addItemListener(applet);
         border.setSelected(false);
+
         randomclr.addItemListener(applet);
         randomclr.setSelected(false);
 
-        //Add you button to the button-panel.
+        //Add buttons to the button-panel.
         buttonPanel.add(buttonCreate);
         buttonPanel.add(randomLinesButton);
         buttonPanel.add(vectorButton);
@@ -238,59 +242,62 @@ public class Application {
         buttonPanel.add(trianglezButton);
         buttonPanel.add(strokeNColourButton);
         buttonPanel.add(starButton);
-        buttonPanel.add(printButton);
         buttonPanel.add(signatureButton);
 
-
         //Also add buttons/radio-buttons/check-boxes.
-        buttonPanel.add(randomclr); buttonPanel.add(border);
-        buttonPanel.add(Randomize); buttonPanel.add(clearButton); buttonPanel.add(filterButton);
-        buttonPanel.add(saveButton);buttonPanel.add(closeButton);
+        buttonPanel.add(randomclr); /*buttonPanel.add(linear); buttonPanel.add(circular);*/
+        buttonPanel.add(border);
+        buttonPanel.add(Randomize);
+        buttonPanel.add(clearButton);
+        buttonPanel.add(filterButton);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(closeButton);
 
-//store the applet in panel
+        // Store the applet in panel
         panel.add(applet);
-//store the buttonPanel in panel
+
+        // Store the buttonPanel in panel
         frame.add(buttonPanel);
-//store the panel in the frame
+
+        // Store the panel in the frame
         frame.add(panel);
-//assign a size for the frame
-//reading the size from the applet
+
+        // Assign a size for the frame
         frame.setSize(applet.getSize().width, applet.getSize().height);
-//display the frame
+
+        // Display the frame
         frame.setVisible(true);
     }
 
     /***
-     * Returns whether or not the border checkbox is selected.
-     * @return boolean - State of the border checkbox.
+     *
+     * @return Return boolean (border selected)
      */
-    public boolean getBorderState(){
+    public boolean getBorderState() {
         return border.isSelected();
     }
 
     /***
-     * Returns whether or not the randomclr checkbox is selected.
-     * @return boolean - State of the randomclr checkbox.
+     *
+     * @return Return boolean (random colour selected)
      */
-    public boolean getRandomColorState(){
+    public boolean getRandomColorState() {
         return randomclr.isSelected();
     }
 
     /***
-     * Set the state of the randomclr checkbox.
-     * @param b - boolean
+     *
+     * @param b boolean (set random colour state)
      */
-    public void setRandomclrState(boolean b){
+    public void setRandomclrState(boolean b) {
         randomclr.setSelected(b);
     }
 
     /***
-     * HashMap containing all the buttons in the GUI,
-     * and the keys (String values) connected to those buttons.
-     * @return
+     *
+     * @return HashMap (buttons)
      */
-    public HashMap getButtons(){
+    public HashMap getButtons() {
         return buttonMap;
     }
-
 }
