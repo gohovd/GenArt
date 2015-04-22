55e8ac9107086e80d248f250e21a55ff57a9cf62
gohovd <goran.hovde@gmail.com>
2015-04-22 15:33:12 +0200

    Robot velger ikke lenger filter med en gang etter å ha blitt avbrutt og startet på nytt.

--- src/Brush.java
+++ src/Brush.java
@@ -5,15 +5,21 @@ import processing.core.PApplet;
  */
 public class Brush {
 
-    public PApplet p;
-    public int r, g, b, o;
-    public boolean cc;
+    public PApplet p; //PApplet instance from class MyApplet.
+    public int r, g, b, o; //The four ints containing the various color values.
+    public boolean cc; //CC (Custom Color) stores whether or not a custom color should be used.
 
+    /***
+     * Constructor for class Brush.
+     * @param input PApplet - instance from MyApplet.
+     */
     public Brush(PApplet input){
         p = input;
     }
 
     /***
+     * Sets the color for all brushes (art-methods), by taking
+     * in four integers as parameters.
      *
      * @param red int - Degree of red (0-255)
      * @param green int - Degree of green (0-255)
@@ -27,10 +33,21 @@ public class Brush {
         o = opacity;
     }
 
+    /***
+     * CC stands for custom color, and is a boolean that keeps track
+     * of whether or not the user/robot wants a 'custom color' or not.
+     * When set to true, the draw/display methods in sub-classes do not
+     * choose a random color, but rather nuances of the user defined color.
+     *
+     * @param b boolean - CC(Custom Color)
+     */
     public void setCC(Boolean b){
         cc = b;
     }
 
+    /***
+     * Print data about CC and Color status.
+     */
     public void printCC(){
         System.out.println("CC   (" + getClass() + "): " + cc);
         System.out.println("Color(" + getClass() + "): " + r + " " + g + " " + b + " " + o);
