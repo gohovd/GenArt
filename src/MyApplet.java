fd8c8322acd27eaa3d9d9855c9df9b23e0bf088f
Urdar <jens.vingen@gmail.com>
2015-04-22 21:02:25 +0200

    Kan nå slette input (backspace) på save og signature

--- src/MyApplet.java
+++ src/MyApplet.java
@@ -445,6 +445,10 @@ public class MyApplet extends PApplet implements ActionListener, ItemListener {
                 // Otherwise, concatenate the String
                 // Each character typed by the user is added to the end of the String variable.
                 typing = typing + key;
+
+                if (key == '\u0008'){
+                    typing="";
+                }
             }
 
 
@@ -505,6 +509,7 @@ public class MyApplet extends PApplet implements ActionListener, ItemListener {
             // If the return key is pressed, save the String and clear it
 
             if (key == '\n') {
+
                 saved = typing;
                 // A String can be cleared by setting it equal to ""
                 typing = "";
@@ -534,9 +539,14 @@ public class MyApplet extends PApplet implements ActionListener, ItemListener {
                     saveButton = false;
                 }
             } else {
+
                 // Otherwise, concatenate the String
                 // Each character typed by the user is added to the end of the String variable.
                 typing = typing + key;
+
+                if (key == '\u0008'){
+                    typing="";
+                }
             }
         } else if (randomize) {
 
